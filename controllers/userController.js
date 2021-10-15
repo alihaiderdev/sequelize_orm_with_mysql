70656271680e76829409f4d02314df84ec4637a0
alihaiderdev <ah1675179@gmail.com>
2021-10-15 19:49:05 +0500

    Done with operators, insert, delete, update, read.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,9 @@
 // const { where } = require('sequelize/types');
 var db = require('../models/index');
-const { Sequelize } = require('sequelize');
+const { Sequelize, Op } = require('sequelize');
 const Users = db.users;
+
+// note if we use findOne and findAll with out any parameters then we can simlpy use it like findOne() and  findAll()
 var addUser = async (req, res) => {
   //   let data = await Users.build({ name: 'rehman', email: 'rehman@gmail.com' });
   //   await data.save();
@@ -23,7 +25,7 @@ var addUser = async (req, res) => {
 
 var crudOperation = async (req, res) => {
   // insert
-  //   let data = await User.create({ name: 'rehman', gender: 'male', email: 'rehman@gmail.com' });
+  //   let data = await Users.create({ name: 'rehman', gender: 'male', email: 'rehman@gmail.com' });
   //   console.log('data : ', data.id);
 
   // insert
@@ -55,14 +57,98 @@ var crudOperation = async (req, res) => {
 
 var queryData = async (req, res) => {
   // insert
-  let data = await User.create({ name: 'rehman', gender: 'male', email: 'rehman@gmail.com' }, { fields: ['email', 'gender'] });
-  console.log('data : ', data.id);
+  // let data = await Users.create({ name: 'rehman', gender: 'male', email: 'rehman@gmail.com' }, { fields: ['email', 'gender'] });
+  // console.log('data : ', data.id);
 
   // select
-  // let data = await User.findAll({ attributes: ['email', 'gender'] }); // getting all data with only that fields we passing in array
-  // let data = await User.findAll({ attributes: [['email', 'emailID'], 'gender'] }); // getting all data with only that fields we passing in array and also renaming it
+  // let data = await Users.findAll({});
+  // let data = await Users.findAll({ attributes: ['email', 'gender'] }); // getting all data with only that fields we passing in array
+  // let data = await Users.findAll({ attributes: [['email', 'emailID'], 'gender'] }); // getting all data with only that fields we passing in array and also renaming it
 
-  res.status(200).json({ message: 'queryData' });
+  // getting all data with only that fields we passing in array and also renaming it and count the no of records using email
+  // let data = await Users.findAll({
+  //   attributes: ['name', ['email', 'emailID'], 'gender', [Sequelize.fn('Count', Sequelize.col('email')), 'emailCount']], // here count s case sensitive means count, COUNT, Count all are same
+  // });
+
+  // getting all data with only that fields we passing in array and also renaming it and concat it with anything that we want
+  // let data = await Users.findAll({
+  //   attributes: ['name', ['email', 'emailID'], 'gender', [Sequelize.fn('concat', Sequelize.col('email'), 'ID'), 'emailConcat']],
+  // });
+
+  // include / exclude
+  // let data = await Users.findAll({
+  //   attributes: { exclude: 'createdAt' }, // exclude single value
+  // });
+  // let data = await Users.findAll({
+  //   attributes: { exclude: ['createdAt', 'updatedAt'] }, // exclude mutiple values
+  // });
+
+  // exclude and include at a time
+  // let data = await Users.findAll({
+  //   attributes: {
+  //     exclude: ['createdAt', 'updatedAt'],
+  //     include: [[Sequelize.fn('concat', 'Mr ', Sequelize.col('name'), ' ', Sequelize.col('email')), 'fullName']],
+  //   },
+  // });
+
+  // conditions
+  // let data = await Users.findAll({ where: { id: 63 } });
+  // OR
+  // let data = await Users.findAll({ where: { id: { [Op.eq]: 63 } } });
+
+  // operators
+  // let data = await Users.findAll({ where: { id: { [Op.gte]: 1 } } });
+  // let data = await Users.findAll({ where: { email: { [Op.like]: '%.com' } } }); // get all records that email ends at .com
+  // let data = await Users.findAll({ where: { email: { [Op.like]: 'r%' } } }); // get all records that email starts from r
+  // let data = await Users.findAll({ where: { email: { [Op.like]: '%r%' } } }); // get all records that email contains r
+  // let data = await Users.findAll({ where: { id: { [Op.gt]: 3 }, email: { [Op.like]: '%r%' } } }); // get all records that email contains r
+
+  // orderBy
+  // let data = await Users.findAll({ where: { id: { [Op.gt]: 1 }, email: { [Op.like]: '%.com' } }, order: [['name', 'desc']] }); // get all records that email ends at .com and groupby descending order
+  // let data = await Users.findAll({
+  //   where: { id: { [Op.gt]: 1 }, email: { [Op.like]: '%.com' } },
+  //   order: [
+  //     ['name', 'desc'], // desc and asc case sensitive
+  //     ['email', 'asc'],
+  //   ],
+  // }); // get all records that email ends at .com and orderby descending order
+
+  // limit
+  // let data = await Users.findAll({
+  //   where: { id: { [Op.gt]: 1 }, email: { [Op.like]: '%.com' } },
+  //   order: [
+  //     ['name', 'desc'], // desc and asc case sensitive
+  //   ],
+  //   limit: 2,
+  // }); // get all records that email ends at .com and orderby descending order and limit it
+
+  // offset
+  // let data = await Users.findAll({
+  //   where: { id: { [Op.gt]: 1 }, email: { [Op.like]: '%.com' } },
+  //   order: [
+  //     ['name', 'desc'], // desc and asc case sensitive
+  //   ],
+  //   limit: 2,
+  //   offset: 1,
+  // }); // get all records that email ends at .com and orderby descending order and limit it
+
+  // groupby
+  // let data = await Users.findAll({
+  //   where: { id: { [Op.gt]: 1 }, email: { [Op.like]: '%.com' } },
+  //   order: [
+  //     ['name', 'desc'], // desc and asc case sensitive
+  //   ],
+  //   // group: ['email'],
+  //   group: ['name', 'email'],
+  //   limit: 2,
+  //   offset: 1,
+  // }); // get all records that email ends at .com and orderby descending order and limit it
+
+  // count
+  // let data = await Users.count({});
+  // OR;
+  let data = await Users.count();
+  res.status(200).json({ data });
 };
 
 module.exports = { addUser, crudOperation, queryData };
--- models/index.js
+++ models/index.js
@@ -10,7 +10,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('sequelize_with_mysql', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: false, // for stopping insertion sql queries logs
+  logging: true, // for stopping insertion sql queries logs
   pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
 });
 // OR;
